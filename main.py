93c7f8f804635e648a79adea8e33c7439e2d531a
agaget-cap <antoine.gaget@gmail.com>
2022-08-31 17:30:06 +0200

    OK version, console line version.

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import photosorterlib
+import sys
 
 
 def main():
-    photosorterlib.test()
+    input_folder = None
+    output_folder = None
+
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "--help":
+            print("Usage : python3 main.py -in INPUT_FOLDER -out OUTPUT_FOLDER [--suppress_warnings|-sw] [--verbose]")
+            print("     -in : the input folder. Can have nested folders.")
+            print("     -out : the output folder. Can already contain folders.")
+            print("     --suppress_warnings|-sw : If enabled, do not display warning messages in console.")
+            print("     --verbose : if enabled, will display progression in console.")
+            return
+
+        if sys.argv[i] in ["-in"]:
+            input_folder = sys.argv[i + 1]
+            i += 1
+        if sys.argv[i] in ["-out"]:
+            output_folder = sys.argv[i + 1]
+            i += 1
+        if sys.argv[i] in ["--suppress_warnings", "-sw"]:
+            photosorterlib.SUPPRESS_WARNINGS = True
+        if sys.argv[i] in ["--verbose"]:
+            photosorterlib.VERBOSE = True
+
+    if input_folder is None:
+        input_folder = input("Please enter your input folder :")
+    if output_folder is None:
+        output_folder = input("Please enter your output folder :")
+
+    if photosorterlib.VERBOSE:
+        print(f"Copying and ordering photos from {input_folder} to {output_folder}.")
+
+    photosorterlib.sorter_starter(photosorterlib.get_photo_files(photosorterlib.get_all_files_from(input_folder)),
+                                  output_folder)
 
 
 if __name__ == '__main__':
--- photosorterlib.py
+++ photosorterlib.py
@@ -1,5 +1,23 @@
+import sys
+
 from PIL import Image
 from PIL.ExifTags import TAGS
+import os
+import re
+from datetime import datetime
+import warnings
+import shutil
+import time
+import numpy as np
+
+
+SUPPRESS_WARNINGS = True
+VERBOSE = False
+
+VALID_FILE_EXTENSIONS = [".PNG", ".png", ".JPG", ".jpg"]
+VALID_FILE_EXTENSIONS_REGEX = []
+for exten in VALID_FILE_EXTENSIONS:
+    VALID_FILE_EXTENSIONS_REGEX.append('\\' + f"{exten}$")
 
 
 def get_meta_data(image):
@@ -21,15 +39,90 @@ def get_meta_data(image):
         data = exif_data.get(tag_id)
         # decode bytes
         if isinstance(data, bytes):
-            data = data.decode()
+            try:
+                data = data.decode()
+            except UnicodeDecodeError as e:
+                if not SUPPRESS_WARNINGS:
+                    warnings.warn(f"{info_dict['Filename']}'s exif data {tag} cannot be decoded")
+                # print(info_dict['Filename'])
+                # print(tag)
         info_dict[tag] = data
 
     return info_dict
 
 
-def test():
-    image_path = "./Photos/IMG_0002.JPG"
-    image = Image.open(image_path)
+def get_all_files_from(dirpath):
+    lres = []
+    for subdir, dirs, files in os.walk(dirpath):
+        for file in files:
+            lres.append(os.path.join(subdir, file).replace('\\', '/'))
+    return lres
 
-    md = get_meta_data(image)
-    print(md)
+
+def get_photo_files(file_list):
+    new_list = []
+
+    for file in file_list:
+        passed = False
+        for regex in VALID_FILE_EXTENSIONS_REGEX:
+            match = re.search(regex, file)
+            if match:
+                passed = True
+                break
+        if passed:
+            new_list.append(file)
+
+    return new_list
+
+
+def copy_file_to_new_folder(file_name, output_dir):
+    image = Image.open(file_name)
+    exifdata = get_meta_data(image)
+    image.close()
+
+    photo_date = datetime.strptime(exifdata['DateTime'], "%Y:%m:%d %H:%M:%S")
+
+    year_folder = f"{photo_date.year}"
+    month_folder = f"{photo_date.strftime('%B')}"
+    output_folder = f"{year_folder}/{month_folder}"
+
+    # if not os.path.exists(f"{output_dir}/{year_folder}"):
+    #     os.makedirs(f"{output_dir}/{year_folder}")
+    if not os.path.exists(f"{output_dir}/{output_folder}"):
+        os.makedirs(f"{output_dir}/{output_folder}")
+
+    shutil.copy2(file_name, f"{output_dir}/{output_folder}")
+
+
+def sorter(photo_file_list, output_dir):
+    for pf in photo_file_list:
+        copy_file_to_new_folder(pf, output_dir)
+
+
+def sorter_verbose(photo_file_list, output_dir):
+    nb_file = len(photo_file_list)
+    file_processed = 0
+
+    times = []
+
+    print(f"Processing {nb_file} files...")
+
+    for pf in photo_file_list:
+        _to_print = f"\rProcessing {pf}"
+        t = time.time()
+        copy_file_to_new_folder(pf, output_dir)
+        duration = time.time() - t
+
+        times.append(duration)
+
+        file_processed += 1
+        _to_print += f" --- {round(float(file_processed)/float(nb_file) * 100.0, 2)}%"
+        _to_print += f" --- {round(np.mean(times) * (nb_file-file_processed), 2)}s remaining"
+        print(f"\r{_to_print}", end="")
+
+
+def sorter_starter(photo_file_list, output_dir):
+    if VERBOSE:
+        sorter_verbose(photo_file_list, output_dir)
+    else:
+        sorter(photo_file_list, output_dir)
